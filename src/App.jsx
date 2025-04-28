9663307ace41e48cc544407442c316ac7b468f0c
Justice Adam <justiceadam673@gmail.com>
2025-04-28 12:17:17 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium minima
+      qui nesciunt veritatis excepturi, velit commodi ea eaque repellendus
+      doloribus rerum rem facilis quisquam pariatur iusto. Ipsa aliquid iusto,
+      nihil harum in amet vero quia reiciendis quaerat, deleniti earum autem
+      sapiente reprehenderit iste, velit commodi modi magnam quis porro ea ut
+      tenetur fugit beatae quisquam. Repellat ut ad architecto eaque dolorum
+      ipsa animi totam vero dolorem incidunt veniam, quibusdam vel omnis
+      necessitatibus sint quos nam accusamus magnam voluptatibus eveniet
+      aspernatur minima. Possimus consequatur accusantium quos dolores, enim
+      repudiandae dolore ipsam eius. Quo id illo obcaecati voluptate asperiores
+      ipsa omnis nulla!
+    </>
+  );
+}
+
+export default App;
